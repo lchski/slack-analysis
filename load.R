d48f1b425a558f3e6121e0e51779e6f46d1c9226
Lucas Cherkewski <lucas.cherkewski@cds-snc.ca>
2020-08-12 17:32:12 -0400

    Convert username mentions to their names for easier reading

--- load.R
+++ load.R
@@ -25,7 +25,15 @@ load_message_file <- function(file_path_to_load) {
       profile = starts_with("profile_")
     )
 }
-  
+
+user_ids_real_names <- users %>%
+  select(id, name, real_name) %>%
+  mutate(use_name = ifelse(is.na(real_name), name, real_name)) %>%
+  select(id, use_name) %>%
+  mutate(id = paste0("<@", id, ">")) %>%
+  mutate(use_name = paste0("<@", use_name, ">")) %>%
+  deframe()
+
 messages <- fs::dir_ls(conversation_file_prefix, glob = "*.json", recurse = TRUE, type = "file") %>%
   .[! . %in% paste0(conversation_file_prefix, c("users.json", "channels.json", "integration_logs.json"))] %>% ## filter out the top-level non-message files
   map_dfr(load_message_file, .id = "source_file_path") %>%
@@ -36,7 +44,8 @@ messages <- fs::dir_ls(conversation_file_prefix, glob = "*.json", recurse = TRUE
   mutate(datetime = as_datetime(as.integer(ts))) %>%
   mutate(root_msg_ts = ifelse(is.na(thread_ts), ts, thread_ts)) %>%
   mutate(is_thread_start = ! is.na(reply_count)) %>%
-  mutate(is_thread_reply = ! is.na(thread_ts) & ! is_thread_start)
+  mutate(is_thread_reply = ! is.na(thread_ts) & ! is_thread_start) %>%
+  mutate(text = str_replace_all(text, user_ids_real_names))
 
 msgs <- messages %>%
   select(conversation, root_msg_ts, ts, datetime, type, subtype, user, text, is_thread_start, is_thread_reply, reply_count) %>%
