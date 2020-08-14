eb8bebfd69f2f7902db32216508b7443a96ff5ad
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-08-13 20:52:28 -0400

    Bring image URLs / <img> tags into the text

--- analysis/en.R
+++ analysis/en.R
@@ -14,6 +14,21 @@ en_msgs <- msgs %>%
 
 en_msgs %>%
   arrange(conversation, root_msg_ts, datetime) %>%
-  mutate(text = ifelse(is_thread_reply, paste("->", text), text)) %>%
-  select(conversation, datetime, user_use_name, text) %>%
+  #mutate(text = ifelse(is_thread_reply, paste("->", text), text)) %>%
+  mutate(text_with_images = if_else(
+    is.na(image_urls_html),
+    text,
+    paste0(text, "\n\n", image_urls_html)
+  )) %>%
+  select(
+    conversation,
+    datetime,
+    user_use_name,
+    text,
+    text_with_images,
+    image_urls_text,
+    image_urls_html,
+    is_thread_start,
+    is_thread_reply
+  ) %>%
   write_csv("data/out/en-msgs.csv")
